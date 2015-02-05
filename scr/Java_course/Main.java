e8fed016ac92e1f4cfb1cd1d909182f670bc4ebe
zeppiery <Nastya_bm@ukr.net>
2015-02-05 11:54:40 +0200

    Update Main.java

--- scr/Java_course/Main.java
+++ scr/Java_course/Main.java
@@ -1,5 +1,7 @@
-public class NewMain {
-  public static void main(String args[]) {
+package Java_course;
+
+public class Main {
+  public static void main(String[] args) {
      for (int i = 0; i < args.length; i++) {
      System.out.println("Аргумент = " + args [i]);
     }
