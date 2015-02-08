05677a8a66c06bd15c0823243775b1a3622ae0ab
wasman <kislo.metal@gmail.com>
2015-02-08 20:14:48 +0200

    formatting & correcting

--- scr/Java_course/Main.java
+++ scr/Java_course/Main.java
@@ -1,9 +1,12 @@
 package Java_course;
 
 public class Main {
-  public static void main(String[] args) {
-     for (int i = 0; i < args.length; i++) {
-     System.out.println("Аргумент = " + args [i]);
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("Аргумент = " + arg);
+        }
+
+        System.out.println("Количество= " + args.length);
     }
-    System.out.println("Количество= " + args.length;);
-  }
+}
